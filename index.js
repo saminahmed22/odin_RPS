79f4bc33c01c8f3726df911fe4913b36177dd527
Samin Ahmed <saminahmed1001@gmail.com>
2024-12-31 19:55:18 +0600

    refactor[index.js]: finilized the ui

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const choice = button.id;
+            playGame(choice);                
+        });
+});
+
+const div = document.querySelector("div");
+
 const option_values = {rock: 1, paper: 2, scissor: 3};
 const option_values_reversed = {1: "Rock", 2: "Paper", 3:"Scissor"};
 
@@ -11,81 +21,73 @@ computer_score = 0;
 
 function playRound(human_choice, computer_choice){
 
-    console.log("Round: " + round);
-    console.log("Human Score: " + human_score);
-    console.log("Computer Score: " + computer_score);
+    div.innerHTML += "Round: " + round + "<br>";
+    div.innerHTML += "Human Score: " + human_score + "<br>";
+    div.innerHTML += "Computer Score: " + computer_score + "<br>";
     
     human_choice_converted = option_values[human_choice];
-    console.log("Human choosed: " + option_values_reversed[human_choice_converted]);
-    console.log("Computer choosed: " + option_values_reversed[computer_choice])
+
+    div.innerHTML += "Human choosed: " + option_values_reversed[human_choice_converted] + "<br>";
+    div.innerHTML += "Computer choosed: " + option_values_reversed[computer_choice] + "<br>";
 
     if(human_choice_converted === computer_choice){
-        console.log("Draw");
-        console.log("No points were given");
+        div.innerHTML += "Draw" + "<br>";
+        div.innerHTML += "No points were given" + "<br>";
     }
     else if(human_choice_converted === 1 && computer_choice === 3){
-        console.log("Human wins!");
-        console.log("Human got 1 point");
+        div.innerHTML += "Human wins!" + "<br>";
+        div.innerHTML += "Human got 1 point" + "<br>";
         human_score += 1;
     }
     else if(human_choice_converted === 3 && computer_choice === 1){
-        console.log("Computer wins!");
-        console.log("Computer got 1 point");
+        div.innerHTML += "Computer wins!" + "<br>";
+        div.innerHTML += "Computer got 1 point" + "<br>";
         computer_score += 1;
     }
     else{
         if(human_choice_converted > computer_choice){
-            console.log("Human wins!");
-            console.log("Human got 1 point");
+            div.innerHTML += "Human wins!" + "<br>";
+            div.innerHTML += "Human got 1 point" + "<br>";
             human_score += 1;
         }
         else{
-            console.log("Computer wins!");
-            console.log("Computer got 1 point");
+            div.innerHTML += "Computer wins!" + "<br>";
+            div.innerHTML += "Computer got 1 point" + "<br>";
             computer_score += 1;
         }
     }
 }
 
 function playGame(human_selection){
-    if(round <= 5){
-        if(round < 5){
-            let computer_selection = getComputerChoice();
-            playRound(human_selection, computer_selection);     
-            round += 1;
-        }
-        else if(round === 5){
-            let computer_selection = getComputerChoice();
-            playRound(human_selection, computer_selection);
-            round += 1;     
-            result();
-            console.log("***GAME OVER***");
-            console.log("Please refresh the page to restart");
-        };
+    if(human_score === 5 || computer_score === 5){
+        div.innerHTML += "<br>";
+        div.innerHTML += "<br>";  
+        result();
+        return;
     }
-    
-}
+    else{
+        div.innerHTML = "";
+        let computer_selection = getComputerChoice();
+        playRound(human_selection, computer_selection);     
+        round += 1;
+    };   
+};
 
 function result(){
 
-    console.log("Human final score: " + human_score);
-    console.log("Computer final score: " + computer_score);
+    div.innerHTML += "Human final score: " + human_score + "<br>";
+    div.innerHTML += "Computer final score: " + computer_score + "<br>";
 
     if(human_score === computer_score){
-        console.log("The game ends in a draw!");
+        div.innerHTML += "The game ends in a draw!" + "<br>";
     }
     else if (human_score > computer_score){
-        console.log("Human wins the game with " + human_score + " points!")
+        div.innerHTML += "Human wins the game with " + human_score + " points!" + "<br>";
     }
     else{
-        console.log("Computer wins the game with " + computer_score + " points!")
+        div.innerHTML += "Computer wins the game with " + computer_score + " points!" + "<br>";
     }
+    div.innerHTML += "***GAME OVER***" + "<br>";
+    div.innerHTML += "Please refresh the page to restart" + "<br>";
 };
 
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-        button.addEventListener("click", () => {
-            const choice = button.id;
-            playGame(choice);                
-        });
-});
